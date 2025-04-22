f3978a02868b532b7e9468b24e5182781c59dc3d
Valentin <61027346+Vaalley@users.noreply.github.com>
2025-04-22 15:55:59 +0200

    Implement IGDB game search endpoint and add token logging in dev mode

--- handlers/igdb.ts
+++ handlers/igdb.ts
@@ -1,8 +1,38 @@
 import { Context } from "hono";
+import { getEnv } from "../config/config.ts";
+import { logger } from "../utils/logger.ts";
 
-export function search(c: Context) {
+const BASE_URL = "https://api.igdb.com/v4";
+
+export async function search(c: Context) {
+	const { query } = await c.req.query();
+
+	logger.info("🔍 IGDB search query: " + query);
+
+	const response = await fetch(`${BASE_URL}/games`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Client-ID": getEnv("IGDB_CLIENT_ID"),
+			"Authorization": `Bearer ${getEnv("IGDB_ACCESS_TOKEN")}`,
+		},
+		body: JSON.stringify({
+			search,
+		}),
+	});
+
+	if (!response.ok) {
+		return c.json({
+			success: false,
+			message: "Failed to search",
+		});
+	}
+
+	const data = await response.json();
+
 	return c.json({
 		success: true,
 		message: "search results",
+		data,
 	});
 }
